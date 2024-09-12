abbc8726fb577a545551e71be201be04f502d077
myself-Irfan <ahmed.1995.irfan@gmail.com>
2024-09-12 15:53:16 +0600

    Day 28 Complete

--- 28-214.py
+++ 28-214.py
@@ -38,30 +38,44 @@ class WindowI(Tk):
         self.title('Pomodoro')
         self.config(padx=100, pady=50, bg=YELLOW)
         self.cycle = 0
+        self.check_li = ''
+        self.timer_id = None
 
-    def count_down(self, canvas: CanvasT, label_timer: LabelI, timer_txt, count):
+    def count_down(self, canvas: CanvasT, label_timer: LabelI, timer_txt, count, check_mark):
         count_min = count // 60
         count_sec = count % 60
 
         canvas.itemconfig(timer_txt, text=f'{count_min:02}:{count_sec:02}')
         if count > 0:
-            self.after(1000, self.count_down, canvas, label_timer, timer_txt, count - 1)
+            self.timer_id = self.after(1000, self.count_down, canvas, label_timer, timer_txt, count - 1, check_mark)
         else:
             if self.cycle < 8:
-                self.start_timer(canvas, label_timer, timer_txt)
+                self.start_timer(canvas, label_timer, timer_txt, check_mark)
+                if self.cycle % 2 == 0:
+                    self.check_li += '✔'
+                    check_mark.config(text=self.check_li)
 
-    def start_timer(self, canvas: CanvasT, label_timer: LabelI, timer_txt):
+    def start_timer(self, canvas: CanvasT, label_timer: LabelI, timer_txt, check_mark):
         self.cycle += 1
 
         if self.cycle == 8:
-            label_timer.config(text='LONG BREAK')
-            self.count_down(canvas, label_timer, timer_txt, LONG_BREAK_MIN)
+            label_timer.config(text='BREAK', fg=RED)
+            self.count_down(canvas, label_timer, timer_txt, LONG_BREAK_MIN, check_mark)
         elif self.cycle % 2 == 0:
-            label_timer.config(text='SHORT BREAK')
-            self.count_down(canvas, label_timer, timer_txt,SHORT_BREAK_MIN)
+            label_timer.config(text='BREAK', fg=PINK)
+            self.count_down(canvas, label_timer, timer_txt, SHORT_BREAK_MIN, check_mark)
         else:
-            label_timer.config(text='WORK')
-            self.count_down(canvas, label_timer, timer_txt,WORK_MIN)
+            label_timer.config(text='WORK', fg=GREEN)
+            self.count_down(canvas, label_timer, timer_txt, WORK_MIN, check_mark)
+
+    def reset_timer(self, canvas, timer_txt, label, check_mark):
+        self.cycle = 0
+        self.check_li = ''
+        check_mark.config(text=self.check_li)
+        if self.timer_id:
+            self.after_cancel(self.timer_id)
+        canvas.itemconfig(timer_txt, text='00:00')
+        label.config(text='TIMER', fg=GREEN)
 
 
 def main():
@@ -72,17 +86,17 @@ def main():
     i_canvas.attach_img(t_photo)
     timer_txt = i_canvas.attach_txt('00:00')
 
-    label_timer = LabelI('Timer', GREEN, YELLOW)
+    label_timer = LabelI('TIMER', GREEN, YELLOW)
     label_timer.set_grid_pos(1,0)
-
-    start_btn = ButtonI('Start', command=lambda: i_window.start_timer(i_canvas, label_timer, timer_txt))
-    start_btn.set_grid_pos(2,0)
-
-    reset_btn = ButtonI('Reset')
-    reset_btn.set_grid_pos(2,2)
 
     check_mark = LabelI('', GREEN, YELLOW)
     check_mark.set_grid_pos(1,3)
+
+    start_btn = ButtonI('Start', command=lambda: i_window.start_timer(i_canvas, label_timer, timer_txt, check_mark))
+    start_btn.set_grid_pos(2,0)
+
+    reset_btn = ButtonI('Reset', command=lambda: i_window.reset_timer(i_canvas, timer_txt, label_timer, check_mark))
+    reset_btn.set_grid_pos(2,2)
 
     i_window.mainloop()
 
@@ -93,8 +107,8 @@ if __name__ == '__main__':
     GREEN = "#9bdeac"
     YELLOW = "#f7f5dd"
     FONT_NAME = "Courier"
-    WORK_MIN = 25
+    WORK_MIN = 15
     SHORT_BREAK_MIN = 5
-    LONG_BREAK_MIN = 20
+    LONG_BREAK_MIN = 10
 
     main()
