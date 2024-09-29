d3b7dd6fbc4a9fc595d7b554e8198044e803ff60
myself-Irfan <ahmed.1995.irfan@gmail.com>
2024-09-29 14:31:39 +0600

    Day 30 Complete

--- 29-220.py
+++ 29-220.py
@@ -21,9 +21,10 @@ class PwdManageApp:
         # init entry
         self.e_website = self.create_entry(1)
         self.e_usrmail = self.create_entry(2)
-        self.e_pwd = self.create_entry(3, 18, 1)
+        self.e_pwd = self.create_entry(3)
 
         # init btn
+        self.create_btn('Search Credentials', self.view_creds,1, 2)
         self.create_btn('Generate Password', self.gen_pwd, 3, 2)
         self.create_btn('Add Credentials', self.save_creds, 4, 1, colspan=2, width=36)
 
@@ -60,23 +61,52 @@ class PwdManageApp:
         self.e_pwd.delete(0, END)
         self.e_pwd.insert(0, rand_gen_pwd)
 
+    def view_creds(self):
+        usr_web = self.e_website.get().lower()
+
+        try:
+            with open('29-220.json', 'r') as f:
+                j_in = json.load(f)
+                try:
+                    msg = f'Email: {j_in[usr_web]['email']} \tPassword: {j_in[usr_web]['password']}'
+                except KeyError:
+                    msg = f'{usr_web} not found in records'
+                finally:
+                    messagebox.showinfo(title=f"{usr_web}", message=msg)
+        except FileNotFoundError:
+            messagebox.showerror(title="ERROR!", message='File Not Found')
+
     def save_creds(self):
         usr_web = self.e_website.get()
         usr_info = self.e_usrmail.get()
         usr_pwd = self.e_pwd.get()
 
         new_entry = {
-            f"{usr_web}": {
+            f"{usr_web.lower()}": {
                 "email": usr_info,
                 "password": usr_pwd
             }
         }
 
         if usr_pwd and usr_info and usr_web:
-            if messagebox.askokcancel(title=f"{usr_web}", message=f"Confirm save:\n Username/E-mail: {usr_info}\n Password: {usr_pwd}"):
-                with open('29-220.json', 'a') as f:
-                    json.dump(new_entry, f)
+            if messagebox.askokcancel(title=f"{usr_web}",
+                                      message=f"Confirm save:\n Username/E-mail: {usr_info}\n Password: {usr_pwd}\n"):
+                try:
+                    with open('29-220.json', 'r') as f:
+                        try:
+                            data = json.load(f)
+                        except json.JSONDecodeError:
+                            data = {}
+                except FileNotFoundError:
+                    data = {}
+
+                data.update(new_entry)
+
+                with open('29-220.json', 'w') as f:
+                    json.dump(data, f, indent=4)
+
                 self.empty_entry()
+                messagebox.showinfo(title=f"{usr_web}", message="Credentials Saved")
         else:
             messagebox.showerror(title='ERROR', message=f'Please fill the empty field')
 
