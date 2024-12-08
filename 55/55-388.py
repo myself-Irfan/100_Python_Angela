0df7484a17fedd91c20dce96d5c9a442c61e4422
myself-Irfan <ahmed.1995.irfan@gmail.com>
2024-12-08 23:18:24 +0600

    Day 55 DONE

--- 55/55-388.py
+++ 55/55-388.py
@@ -9,8 +9,15 @@ def get_num() -> int:
     return randint(0, 9)
 
 @app.route('/', methods=['GET'])
-def guess() -> Response:
+def start_guess() -> Response:
     return make_response(jsonify(message='Guess the number between 0 and 9'))
+
+
+@app.route('/show-guess', methods=['GET'])
+def get_guess() -> Response:
+    global num
+    return make_response(jsonify(message=f'The correct number is: {num}'))
+
 
 @app.route('/<int:number>', methods=['POST'])
 def post_guess(number: int) -> Response:
